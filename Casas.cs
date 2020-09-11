727146b7ad6e18d8bffe20ac124527821d60037d
michaelsans <71149396+michaelsans@users.noreply.github.com>
2020-09-11 15:42:07 -0300

    Add files via upload

---
+++ Casas.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Casas : MonoBehaviour
+{
+
+    public Transform[] positions;
+    public GameObject[] rooms;
+    public GameObject target;
+    private float timeBtwRoom;
+    public float startTimeBtwRoom = .10f;
+    public LayerMask whatIsRoom;
+
+
+    void Update()
+    {
+        if (timeBtwRoom <= 0)
+        {
+            transform.position = target.transform.position;
+            Casa();
+            timeBtwRoom = startTimeBtwRoom;
+        }
+        else
+        {
+
+            timeBtwRoom -= Time.deltaTime;
+        }
+
+    }
+
+
+    private void Casa()
+    {
+        Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, whatIsRoom);
+        if (roomDetection == null)
+            {
+            if (transform.position == positions[0].transform.position)
+            {
+                int rand = Random.Range(2, 3);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+                
+            }
+            else if (transform.position == positions[1].transform.position)
+            {
+                int rand = Random.Range(2, 3);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+               
+            }
+            else if (transform.position == positions[2].transform.position)
+            {
+                int rand = Random.Range(2, 3);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+                
+            }
+            else if (transform.position == positions[3].transform.position)
+            {
+                int rand = Random.Range(2, 3);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+               
+            }
+            else if (transform.position == positions[4].transform.position )
+            {
+                int rand = Random.Range(2, 3);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+                
+            }
+            else if (transform.position == positions[8].transform.position)
+            {
+                int rand = Random.Range(2, 3);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+                
+            }
+            else if (transform.position == positions[5].transform.position)
+            {
+                Instantiate(rooms[3], transform.position, Quaternion.identity);
+              
+            }
+            else if (transform.position == positions[6].transform.position)
+            {
+                Instantiate(rooms[3], transform.position, Quaternion.identity);
+                
+            }
+            else if (transform.position == positions[9].transform.position)
+            {
+                Instantiate(rooms[3], transform.position, Quaternion.identity);
+                
+            }
+            else if (transform.position == positions[10].transform.position)
+            {
+                Instantiate(rooms[3], transform.position, Quaternion.identity);
+                
+            }
+
+            else if (transform.position == positions[7].transform.position)
+            {
+                int rand = Random.Range(1, 3);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+                
+            }
+            else if (transform.position == positions[11].transform.position)
+            {
+                int rand = Random.Range(1, 3);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+                
+            }
+            else if (transform.position == positions[13].transform.position)
+            {
+                int rand = Random.Range(0, 3);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+               
+            }
+            else if (transform.position == positions[14].transform.position)
+            {
+                int rand = Random.Range(0, 3);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+                
+            }
+            else if (transform.position == positions[12].transform.position)
+            {
+                int rand = Random.Range(0, 3);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+               
+            }
+            else if (transform.position == positions[15].transform.position)
+            {
+                int rand = Random.Range(0, 3);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+               ;
+            }
+
+        }
+    }
+}
+
+    
+
---
+++ levelGen2.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelGen2 : MonoBehaviour
+{
+    public Transform[] startingPositions;
+    public GameObject[] rooms;
+    
+    
+
+    public float moveAmountx;
+    public float moveAmounty;
+    private int direction;
+    
+
+    private float timeBtwRoom;
+    public float startTimeBtwRoom = .25f;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public bool stopGeneration;
+
+    private void Start()
+    {
+        int randStartingPos = Random.Range(1, startingPositions.Length);
+        transform.position = startingPositions[randStartingPos].position;
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+        direction = Random.Range(1, 6);
+    }
+    private void Update()
+    {
+        
+        if (timeBtwRoom <= 0 && stopGeneration == false)
+        {
+            
+            Move();
+            
+            timeBtwRoom = startTimeBtwRoom;
+
+        }
+        else
+        {
+            
+            timeBtwRoom -= Time.deltaTime;
+        }
+       
+
+    }
+
+
+    private void Move()
+    {
+
+        if (direction == 1 || direction == 2)
+        {
+            if (transform.position.x < maxX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x + moveAmountx, transform.position.y);
+                transform.position = newPos;
+
+                direction = Random.Range(1, 6);
+
+
+                if (direction == 3)
+                {
+                    direction = 2;
+                }
+                else if (direction == 4)
+                {
+                    direction = 5;
+                }
+            }
+            else
+            {
+                direction = 5;
+            }
+        }
+        else if (direction == 3 || direction == 4)
+        {
+            if (transform.position.x > minX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x - moveAmountx, transform.position.y);
+                transform.position = newPos;
+
+                direction = Random.Range(3, 6);
+
+            }
+            else
+            {
+                direction = 5;
+            }
+        }
+        else if (direction == 5)
+        {
+            if (transform.position.y < minY)
+            {
+                Vector2 newPos = new Vector2(transform.position.x, transform.position.y + moveAmounty);
+                transform.position = newPos;
+
+
+                direction = Random.Range(1, 6);
+            }
+            else
+            {
+                Instantiate(rooms[1], transform.position, Quaternion.identity);
+                stopGeneration = true;
+            }
+        }
+
+    } 
+}
+    
+
+
